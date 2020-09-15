6fba5eca9e25379d56e768ea4d1408c66dadfbc9
Donovan <donovanle18@gmail.com>
2020-09-15 15:34:19 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+'use strict';
+function main(){
+  $('#js-shopping-list-form').submit(function(event){
+    event.preventDefault();
+    const listItem = $('#shopping-list-entry').val();
+    // eslint-disable-next-line no-console
+    console.log(listItem);
+    $('#shopping-list-entry').val('');
+    if(listItem === ''){
+      $('.shopping-list').append(
+        `<li>
+                    <span class="shopping-item">Fresh Prince of Bel-Air dvd set</span>
+                    <div class="shopping-item-controls">
+                      <button class="shopping-item-toggle">
+                        <span class="button-label">check</span>
+                      </button>
+                      <button class="shopping-item-delete">
+                        <span class="button-label">delete</span>
+                      </button>
+                    </div>
+                  </li>`);
+    } else{
+      $('.shopping-list').append(
+        `<li>
+                    <span class="shopping-item">${listItem}</span>
+                    <div class="shopping-item-controls">
+                      <button class="shopping-item-toggle">
+                        <span class="button-label">check</span>
+                      </button>
+                      <button class="shopping-item-delete">
+                        <span class="button-label">delete</span>
+                      </button>
+                    </div>
+                  </li>`);
+    }
+    
+  });
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
+    
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+}
+$(main());
